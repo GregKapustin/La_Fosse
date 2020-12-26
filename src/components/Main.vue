101c2a99e4ee7ab3706503bb17d14885f0387ba2
Greg <gregory@kapustin.fr>
2020-12-26 14:00:11 +0300

    :tada: Init

---
+++ src/components/Main.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="hello">
+    <div id="scene">
+    </div>
+    <svg id="svg_main" width="1000" height="800">
+      <Zone v-for="(zone, i) in zones"
+        :key="'zone_' + i"
+        :name="zone.name"
+        :path="zone.path"
+        :z="zone.z"
+        :density="zone.density"
+      />
+      <pattern v-for="(p, i) in patterns" :id="'pattern_' + p.density" :key="i" x="0" y="0" :width="grid" height="4" patternUnits="userSpaceOnUse">
+        <rect v-for="j in p.dots" width="2" height="2" :x="j" y="0" fill="black" :key="'pattern_' + p.density + 'dot_' + j" />
+      </pattern>
+    </svg>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+import Zone from './Zone'
+import zones from '../zones'
+
+export default {
+  name: 'Main',
+  components: {
+    Zone
+  },
+  data: () => {
+    return {
+      zones,
+      grid: 100
+    }
+  },
+  computed: {
+    patterns() {
+      const densities = _.uniq(_.map(zones, "density"))
+      return _.map(densities, density => {
+
+        // Let's do the dots array
+        const dots = []
+        let i = Math.floor(this.grid / density)
+        while (i <= this.grid) {
+          dots.push(i)
+          i += this.grid / density
+        }
+
+        return {
+          density,
+          dots
+        }
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#scene {
+  display: block;
+  width: 100%;
+  height: 100px;
+  border-bottom: solid 2px black;
+}
+#svg_main {
+  position: absolute;
+  left: 50%;
+  margin-left: -500px;
+
+}
+</style>
---
+++ src/components/Zone.vue
@@ -0,0 +1,29 @@
+<template>
+  <path
+    :d="path"
+    :fill="'url(#pattern_' + density + ')'"
+    stroke="silver"
+  />
+</template>
+
+<script>
+export default {
+  name: 'Zone',
+  props: {
+    name: String,
+    path: String,
+    zone: Number,
+    density: Number
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#scene {
+  display: block;
+  width: 100%;
+  height: 100px;
+  border-bottom: solid 2px black;
+}
+</style>
